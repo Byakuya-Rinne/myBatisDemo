aaacf4d12b08844c5fe7f6d792d8ef8a153978c6
Yuki <2353845636@qq.com>
2023-07-21 14:10:23 +0800

    按条件查询

--- src/main/java/com/mapper/BrandMapper.java
+++ src/main/java/com/mapper/BrandMapper.java
@@ -1,11 +1,23 @@
 package com.mapper;
 
 import com.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
-    public List<Brand> selectAll();
+    List<Brand> selectAll();
 
-    public List<Brand> selectById(int id);
+    List<Brand> selectById(int id);
+
+//  条件查询 散装参数 @Param("参数#{ }占位符名称, 用类里的属性名"),
+//  对象参数,
+//  map集合参数
+    List<Brand> selectByCondition(@Param("status")int status, @Param("companyName")String companyName,@Param("brandName")String brandName);
+
+    List<Brand> selectByCondition(Brand brand);
+
+    List<Brand> selectByCondition(Map map);
+
 }
--- src/test/java/com/test/MyBatisTest.java
+++ src/test/java/com/test/MyBatisTest.java
@@ -10,7 +10,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MyBatisTest {
 
@@ -50,6 +52,45 @@ public class MyBatisTest {
         System.out.println(brands);
         sqlSession.close();
 
+    }
+
+
+    @Test
+    public void selectByCondition() throws IOException {
+
+
+        int status = 1;
+        String companyName = "华为";
+        //String brandName = "华为";
+
+        companyName = "%" + companyName + "%";
+        //brandName = "%" + brandName + "%";
+
+        Brand brand = new Brand();
+        brand.setStatus(status);
+        //brand.setBrandName(brandName);
+        brand.setCompanyName(companyName);
+
+        Map map = new HashMap();
+        map.put("status", status);
+        //map.put("brandName",brandName);
+        map.put("companyName", companyName);
+
+
+        //加载mybatis核心配置文件, 获取SqlSessionFactory对象
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //获取SqlSessionFactory对象, 用它执行sql
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+//        List<Brand> brands = brandMapper.selectByCondition(status,companyName,brandName);
+        List<Brand> brands = brandMapper.selectByCondition(brand);
+//        List<Brand> brands = brandMapper.selectByCondition(map);
+
+        System.out.println(brands);
+        sqlSession.close();
     }
 
 
